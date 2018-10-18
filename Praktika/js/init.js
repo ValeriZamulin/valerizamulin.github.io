daf518dfe6ee9404d790b63a0008ad2c38ba892c
Valeri <valeri.zamulin@gmail.com>
2018-10-18 12:08:08 +0300

    switch

--- Praktika/js/init.js
+++ Praktika/js/init.js
@@ -9,3 +9,18 @@ var elem = document.querySelector('.collapsible.expandable');
 var instance = M.Collapsible.init(elem, {
   accordion: false,
 });
+
+
+// Checker in manual part of wizard
+var checker = document.getElementById('sw1');
+// when unchecked or checked, run the function
+checker.onchange = function(){
+if(this.checked){
+instance.open(3);
+} 
+else {
+
+}
+}	
+
+instance.open(0);
