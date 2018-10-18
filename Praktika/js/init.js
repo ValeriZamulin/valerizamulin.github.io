b3331e9e07821fff22b6d79d464a193bdd8195d2
Valeri <valeri.zamulin@gmail.com>
2018-10-18 15:11:02 +0300

    activate switch

--- Praktika/js/init.js
+++ Praktika/js/init.js
@@ -9,18 +9,19 @@ var elem = document.querySelector('.collapsible.expandable');
 var instance = M.Collapsible.init(elem, {
   accordion: false,
 });
-
+// Disable accardion animation
+instance.destroy();
 
 // Checker in manual part of wizard
-var checker = document.getElementById('sw1');
-// when unchecked or checked, run the function
-checker.onchange = function(){
-if(this.checked){
-instance.open(3);
-} 
-else {
-
+function Switcher(i) {
+	document.getElementById('switcher'+i).onchange = function(){
+		if(this.checked){
+			instance.open(i-1);
+		} 
+		else {
+			instance.close(i-1);
+		}
+	}		
 }
-}	
 
-instance.open(0);
+M.toast({html: 'I am a toast'})
