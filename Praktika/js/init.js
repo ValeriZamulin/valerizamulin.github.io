d6ee52b59a397b67fed3c336f047b92e2131fa25
Valeri <valeri.zamulin@gmail.com>
2018-10-19 14:49:08 +0300

    Toasts are working. Problem with switch, need to uncheck checkboxes if switch is disabled

--- Praktika/js/init.js
+++ Praktika/js/init.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 	$('.tap-target').tapTarget();
 });
 
+
+/*********************ACCARDION******************/
 // Collapsible for manual wizard
 var elem = document.querySelector('.collapsible.expandable');
 var instance = M.Collapsible.init(elem, {
@@ -12,7 +14,7 @@ var instance = M.Collapsible.init(elem, {
 // Disable accardion animation
 instance.destroy();
 
-// Checker in manual part of wizard
+// Checker in manual part of wizard - Open accardion if switch is enabled
 function Switcher(i) {
 	document.getElementById('switcher'+i).onchange = function(){
 		if(this.checked){
@@ -24,7 +26,26 @@ function Switcher(i) {
 	}		
 }
 
-// Toasts
-function Toast(name) {
-	M.toast({html: name})
+/*******************TOASTS**********************/
+// Toasts - show toast with info if checked
+function Toast(name, id) {
+	if (document.getElementById(id).checked == true) {
+		var func = "'"+id+"'";
+		var toastHTML = '<span>I am toast content' +name+ '</span><button class="btn-flat toast-action" onclick="unCheck('+func+')">Undo</button>';
+		M.toast({
+			html: toastHTML, 
+			displayLength: 180000, 
+			classes: 'rounded '+id,
+		});
+	}
+	else {
+		var toastElement = document.querySelector('.'+id);
+		var toastInstance = M.Toast.getInstance(toastElement);
+		toastInstance.dismiss();
+	}
+}
+// Uncheck and delete toast if press Undo on toast
+function unCheck(id) {
+    document.getElementById(id).checked = false;
+    Toast('noname', id);
 }
